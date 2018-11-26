3991efe0118c16d59a6367bd91c18faadc8ac64e
Ross Dyson <dyson.ross@gmail.com>
2018-11-26 16:51:57 +0900

    init commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+// Enable HTML template middleware
+app.engine('html', require('ejs').renderFile);
+
+// Enable static CSS styles
+app.use(express.static('styles'));
+
+app.use((req, res, next) => {
+  console.log('Request from:', req.ip);
+  console.log('url:', req.originalUrl);
+  console.log('query:', req.query);
+  console.log('body:', req.body);
+  next();
+});
+
+// reply to request with "Hello World!"
+app.get('/', function (req, res) {
+  res.render('index.html');
+});
+
+app.get('/cat', function (req, res) {
+  res.send('You hit cat');
+});
+
+//start a server on port 80 and log its start to our console
+var server = app.listen(80, function () {
+
+  var port = server.address().port;
+  console.log('Example app listening on port ', port);
+
+});
