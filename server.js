4110b6a32361a2d3693edd78fc85a117cd729b89
Ross Dyson <dyson.ross@gmail.com>
2018-11-26 17:29:16 +0900

    try body parser

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-// create application/json parser
-var jsonParser = bodyParser.json();
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// create application/x-www-form-urlencoded parser
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
+// parse application/json
+app.use(bodyParser.json());
 
 // Enable HTML template middleware
 app.engine('html', require('ejs').renderFile);
