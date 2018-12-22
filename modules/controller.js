2a7060a2bacb96c5a45411120b5744cfde854e1d
Ross Dyson <dyson.ross@gmail.com>
2018-12-22 13:23:31 +0900

    serialport debug

--- modules/controller.js
+++ modules/controller.js
@@ -1,3 +1,35 @@
+// NPM Dependencies
+// const SerialPort = require('serialport');
+// const Readline = require('@serialport/parser-readline');
+//
+// const port = new SerialPort(path, { baudRate: 256000 });
+// const parser = new Readline();
+// port.pipe(parser);
+
+parser.on('data', line => console.log(`> ${line}`));
+
+const controlKey = (control) => {
+  switch(control) {
+    case 'up':
+      return 'f';
+    case 'down':
+      return 'b';
+    case 'left':
+      return 'l';
+    case 'right':
+      return 'r';
+    case 'stop':
+      return 's';
+    default:
+      return null;
+  }
+};
+
 exports.controller = (control) => {
   console.log(`I would move ${control} now`);
+  const executeControl = controlKey(control);
+
+  if (executeControl) {
+    // port.write(executeControl);
+  }
 };
